720d6cf14221fbc0f5cae7dc7eee63307a545435
Lucas Kjaero <lucas@lucaskjaero.com>
2017-06-13 10:39:05 -0700

    Basic preprocessing
    Scale the images and normalize values between -1 and 1.

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,16 @@
+import numpy as np
+
 __author__ = 'Lucas Kjaero'
 
+SCALED_HEIGHT = 75
+SCALED_WIDTH = 75
 
 def process_image(image):
-    # image = (image.astype(float) / 256) - 0.5  # normalize to [-0.5,0.5] to avoid saturation
-    # TODO: should also invert image so convolutional zero-padding doesn't add a "border"?
-    yield image
+    # Resize image to average size
+    resized_image = image.resize((SCALED_WIDTH, SCALED_HEIGHT))
+
+    # Convert image to array and then normalize between -1 and 1
+    image_array = np.array(resized_image)
+    normalized_image_array = (2 * (image_array / 256)) - 1
+
+    return normalized_image_array
